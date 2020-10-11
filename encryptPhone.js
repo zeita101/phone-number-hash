755ac71afe3f25eecbe4f78df7e188ae6da0409d
zeita101 <zeitajb1@gmail.com>
2020-10-11 15:31:59 -0400

    Update encryptPhone.js

--- encryptPhone.js
+++ encryptPhone.js
@@ -3,6 +3,22 @@
  * @param number will always be a string formatted as such '111-223-1123'. Your function should return the first 6 digits as * and only the last 4 shown. Ex:
  * ' 111-223-1123 ' should be ' ***-***-1123 '
  */
+function encryptPhoneNumber(number){ //'number' is a parameter
+    let newNum = '';
+    for (let i = 0; i < number.length; i++) {
+        if (i < 7){
+            if (number[i] != '-'){
+                newNum += '*';
+            }else {
+                newNum += '-';
+            }
+            }else {
+            newNum += number[i];
+        }
 
+    }
+    return newNum
+}
+console.log(encryptPhoneNumber("111-223-1123"));
 
 module.exports = encryptPhoneNumber;
